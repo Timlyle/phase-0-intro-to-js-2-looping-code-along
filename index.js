188506b7d84ee7358392390152157619004d0cd7
Timothy Lyle <itsfreezingouthere@yahoo.com>
2022-01-02 14:03:38 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solutions in this file
+
+const arrayOfGifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+    }
+
+    return gifts;
+}
+
+wrapGifts(arrayOfGifts);
+
+function writeCards(arrayOfNames, eventName) {
+    const cardMessages = [];
+    for (let i = 0; i < arrayOfNames.length; i++) {
+
+        const message = `Thank you, ${arrayOfNames[i]} , for the wonderful ${eventName} gift!`
+        cardMessages.push(message)
+    }
+    return cardMessages
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+writeCards(["Bodhi", "Samip", "Ali"], "christmas");
+
+function countDown(givenNumber) {
+    let countDown = givenNumber;
+    debugger
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+    
+};
+countDown(11);
